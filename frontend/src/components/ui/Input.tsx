ecc12fbf7f034fcaf70a4fa1fb0f46c8c93f98bd
Janvi_13 <janvipatel.jrp@gmail.com>
2026-04-08 19:38:37 +0530

    Create Input.tsx

---
+++ frontend/src/components/ui/Input.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      {...props}
+      className={`w-full px-3 py-2.5 rounded-xl bg-white/5 border border-white/15
+      focus:outline-none focus:ring-2 focus:ring-[var(--primary)] transition ${props.className || ""}`}
+    />
+  );
+}
