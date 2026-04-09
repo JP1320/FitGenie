1ec6c2249197dbdf393703adee21f81b6c1699f8
Janvi_13 <janvipatel.jrp@gmail.com>
2026-04-10 02:25:16 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles.css";
+import App from "./App";
+import "./styles/theme.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
