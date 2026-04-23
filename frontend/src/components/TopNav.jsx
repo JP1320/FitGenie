b42e5ac86dba998fcdc6c7e223e25eebed095b89
Janvi_13 <janvipatel.jrp@gmail.com>
2026-04-23 15:26:15 +0530

    Update TopNav.jsx

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -4,10 +4,8 @@ import { Link } from "react-router-dom";
 export default function TopNav() {
   return (
     <header className="topnav">
-      <Link to="/" className="brand">FitGenie Premium</Link>
-      <nav>
-        <Link to="/intent">Start</Link>
-      </nav>
+      <Link to="/welcome" className="brand">FitGenie</Link>
+      <span className="pill">Premium Flow</span>
     </header>
   );
 }
