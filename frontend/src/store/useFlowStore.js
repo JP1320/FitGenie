109d3f1b6b013bf0d011801ee544bfa577dd8946
Janvi_13 <janvipatel.jrp@gmail.com>
2026-04-23 15:25:14 +0530

    Update useFlowStore.js

--- frontend/src/store/useFlowStore.js
+++ frontend/src/store/useFlowStore.js
@@ -6,7 +6,6 @@ export const useFlowStore = create((set) => ({
 
   intent: "",
   intentSubType: "",
-
   ageRange: "",
   gender: "",
 
@@ -16,56 +15,31 @@ export const useFlowStore = create((set) => ({
   heightRange: "",
 
   scanResult: null,
+  selectedProduct: null,
 
-  style: "",
-  budget: "",
-  fabric: [],
-  fitDetails: { sleeve: "", length: "", fit: "" },
+  filters: {
+    style: "",
+    budget: "",
+    fabric: "",
+    fit: "",
+    sleeve: "",
+    length: ""
+  },
 
   recommendations: null,
-
   serviceType: "",
   ratingFilter: "",
   locationFilter: "",
-
   selectedExpert: null,
 
   deliveryMode: "",
   schedule: "",
-  chatEnabled: false,
 
   fitCard: null,
   trackingStatus: "Accepted",
-  feedback: { fit: 0, service: 0, delivery: 0, photoUrl: "" },
+  feedback: { fit: 0, service: 0, delivery: 0, image: "" },
 
   patch: (payload) => set((s) => ({ ...s, ...payload })),
-  reset: () =>
-    set({
-      userId: "u_live_001",
-      loginMode: "guest",
-      intent: "",
-      intentSubType: "",
-      ageRange: "",
-      gender: "",
-      bodyType: "",
-      size: "",
-      heightCm: "",
-      heightRange: "",
-      scanResult: null,
-      style: "",
-      budget: "",
-      fabric: [],
-      fitDetails: { sleeve: "", length: "", fit: "" },
-      recommendations: null,
-      serviceType: "",
-      ratingFilter: "",
-      locationFilter: "",
-      selectedExpert: null,
-      deliveryMode: "",
-      schedule: "",
-      chatEnabled: false,
-      fitCard: null,
-      trackingStatus: "Accepted",
-      feedback: { fit: 0, service: 0, delivery: 0, photoUrl: "" }
-    })
+  patchFilters: (payload) =>
+    set((s) => ({ filters: { ...s.filters, ...payload } }))
 }));
