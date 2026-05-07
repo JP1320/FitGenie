2ca6fda94be582da6e10ede2e73c87a41d689e85
Janvi_13 <janvipatel.jrp@gmail.com>
2026-05-08 00:38:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import TopNav from "./components/TopNav";
 import Welcome from "./pages/Welcome";
+import LoginEmail from "./pages/LoginEmail";
+import LoginMobile from "./pages/LoginMobile";
 import Intent from "./pages/Intent";
 import BasicProfile from "./pages/BasicProfile";
 import BodyInput from "./pages/BodyInput";
@@ -16,23 +17,22 @@ import Feedback from "./pages/Feedback";
 
 export default function App() {
   return (
-    <>
-      <TopNav />
-      <Routes>
-        <Route path="/" element={<Navigate to="/welcome" replace />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/intent" element={<Intent />} />
-        <Route path="/basic-profile" element={<BasicProfile />} />
-        <Route path="/body-input" element={<BodyInput />} />
-        <Route path="/scanner" element={<Scanner />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/service-selection" element={<ServiceSelection />} />
-        <Route path="/expert-list" element={<ExpertList />} />
-        <Route path="/delivery" element={<Delivery />} />
-        <Route path="/fit-card" element={<FitCard />} />
-        <Route path="/tracking" element={<Tracking />} />
-        <Route path="/feedback" element={<Feedback />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/welcome" replace />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/login/email" element={<LoginEmail />} />
+      <Route path="/login/mobile" element={<LoginMobile />} />
+      <Route path="/intent" element={<Intent />} />
+      <Route path="/basic-profile" element={<BasicProfile />} />
+      <Route path="/body-input" element={<BodyInput />} />
+      <Route path="/scanner" element={<Scanner />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/service-selection" element={<ServiceSelection />} />
+      <Route path="/expert-list" element={<ExpertList />} />
+      <Route path="/delivery" element={<Delivery />} />
+      <Route path="/fit-card" element={<FitCard />} />
+      <Route path="/tracking" element={<Tracking />} />
+      <Route path="/feedback" element={<Feedback />} />
+    </Routes>
   );
 }
