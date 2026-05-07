9a3de1f6d1c5bffe0e7dfbabf05bb7476624dc9d
Janvi_13 <janvipatel.jrp@gmail.com>
2026-05-08 02:08:11 +0530

    Fix App routes to match existing page filenames

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import LoginEmail from "./pages/LoginEmail";
 import LoginMobile from "./pages/LoginMobile";
-import Intent from "./pages/Intent";
-import BasicProfile from "./pages/BasicProfile";
-import BodyInput from "./pages/BodyInput";
+import IntentPage from "./pages/IntentPage";
+import BasicProfilePage from "./pages/BasicProfilePage";
+import SizeBodyPage from "./pages/SizeBodyPage";
 import Scanner from "./pages/Scanner";
 import Store from "./pages/Store";
-import ServiceSelection from "./pages/ServiceSelection";
-import ExpertList from "./pages/ExpertList";
-import Delivery from "./pages/Delivery";
-import FitCard from "./pages/FitCard";
-import Tracking from "./pages/Tracking";
-import Feedback from "./pages/Feedback";
+import ServiceTypePage from "./pages/ServiceTypePage";
+import ExpertsPage from "./pages/ExpertsPage";
+import DeliveryPage from "./pages/DeliveryPage";
+import FitCardPage from "./pages/FitCardPage";
+import TrackingPage from "./pages/TrackingPage";
+import FeedbackPage from "./pages/FeedbackPage";
 
 export default function App() {
   return (
@@ -22,17 +22,17 @@ export default function App() {
       <Route path="/welcome" element={<Welcome />} />
       <Route path="/login/email" element={<LoginEmail />} />
       <Route path="/login/mobile" element={<LoginMobile />} />
-      <Route path="/intent" element={<Intent />} />
-      <Route path="/basic-profile" element={<BasicProfile />} />
-      <Route path="/body-input" element={<BodyInput />} />
+      <Route path="/intent" element={<IntentPage />} />
+      <Route path="/basic-profile" element={<BasicProfilePage />} />
+      <Route path="/body-input" element={<SizeBodyPage />} />
       <Route path="/scanner" element={<Scanner />} />
       <Route path="/store" element={<Store />} />
-      <Route path="/service-selection" element={<ServiceSelection />} />
-      <Route path="/expert-list" element={<ExpertList />} />
-      <Route path="/delivery" element={<Delivery />} />
-      <Route path="/fit-card" element={<FitCard />} />
-      <Route path="/tracking" element={<Tracking />} />
-      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/service-selection" element={<ServiceTypePage />} />
+      <Route path="/expert-list" element={<ExpertsPage />} />
+      <Route path="/delivery" element={<DeliveryPage />} />
+      <Route path="/fit-card" element={<FitCardPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="/feedback" element={<FeedbackPage />} />
     </Routes>
   );
 }
