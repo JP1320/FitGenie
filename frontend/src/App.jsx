b115e1b587b57e980586f0dcbcabeb53c77658b6
Janvi_13 <janvipatel.jrp@gmail.com>
2026-07-07 01:10:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import LoginEmail from "./pages/LoginEmail";
-import LoginMobile from "./pages/LoginMobile";
 import IntentPage from "./pages/IntentPage";
 import BasicProfilePage from "./pages/BasicProfilePage";
 import SizeBodyPage from "./pages/SizeBodyPage";
@@ -22,23 +21,20 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/welcome" replace />} />
-
       <Route path="/welcome" element={<Welcome />} />
+
       <Route path="/login/email" element={<LoginEmail />} />
-      <Route path="/login/mobile" element={<LoginMobile />} />
+      <Route path="/login/mobile" element={<Navigate to="/welcome" replace />} />
 
       <Route path="/intent" element={<IntentPage />} />
       <Route path="/basic-profile" element={<BasicProfilePage />} />
       <Route path="/size-body" element={<SizeBodyPage />} />
       <Route path="/camera-scan" element={<Scanner />} />
-
       <Route path="/guided-filters" element={<GuidedFiltersPage />} />
       <Route path="/recommendations" element={<RecommendationsPage />} />
-
       <Route path="/service-type" element={<ServiceTypePage />} />
       <Route path="/quality-location" element={<QualityLocationPage />} />
       <Route path="/experts" element={<ExpertsPage />} />
-
       <Route path="/delivery" element={<DeliveryPage />} />
       <Route path="/fit-card" element={<FitCardPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
